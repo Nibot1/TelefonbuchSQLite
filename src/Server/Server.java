1009618b4baf0ca91d3c58a0fdce4d0c4c1ab8a8
Tobin Rosenau <tobin.rosenau@mail.de>
2017-12-12 18:15:49 +0100

    added Server.java

---
+++ src/Server/Server.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package Server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+/**
+ * @author Tobin Rosenau
+ *
+ */
+public class Server {
+
+	/**
+	 * @param args
+	 */
+	static ServerSocket anschluss;
+	public static void main(String[] args) throws IOException{
+		// Start ServerSocket on port 6000
+		try {
+		 anschluss = new ServerSocket(6000);
+		}catch(BindException e) {
+			e.printStackTrace();
+		}
+		//Starts an endless loop for listening for Client Input
+		  while (true) {
+		// Start listening
+		    Socket lauschen=anschluss.accept();
+		// Print Welcome Message
+		    System.out.println("Ich lausche.");
+		// Read the Client Input
+		    InputStreamReader PortLeser=new
+		    InputStreamReader(lauschen.getInputStream());
+		    BufferedReader Eingabe= new BufferedReader(PortLeser);
+		    String S=Eingabe.readLine();
+	   // Print the Input of the Client
+		    PrintWriter Ausgabe= new PrintWriter(lauschen.getOutputStream(),true);
+		    Ausgabe.println("Ebenfalls " + S);
+		    Ausgabe.close();
+		  }
+		}
+	
+	//Closes Server
+	public void closeServer() {
+		try {
+			anschluss.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	}
+
+
